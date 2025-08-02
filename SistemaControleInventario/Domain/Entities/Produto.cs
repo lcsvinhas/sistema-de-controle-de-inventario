e3def83a31cfecda9a63df51feb946e3a929128e
lcsvinhas <l.vinhas.lv@gmail.com>
2025-08-01 21:22:37 -0300

    feat: criar entidade Produto

---
+++ SistemaControleInventario/Domain/Entities/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaControleInventario.Domain.Entities
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+        public bool Ativo { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public int Estoque { get; private set; }
+        public int EstoqueMinimo { get; private set; }
+    }
+}
